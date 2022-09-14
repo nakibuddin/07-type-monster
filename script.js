d0c3ade272b94c457e4748d3eaacd626b70ccbfb
nakibuddin <nakibaust137@gmail.com>
2022-09-15 05:47:54 +0600

    responsive added for mobile and tab

--- script.js
+++ script.js
@@ -46,8 +46,7 @@ const typeController = (e) => {
     display.innerHTML += `<span class="green">${newLetter === " " ? "▪" : newLetter}</span>`;
   } else {
     display.innerHTML += `<span class="red">${newLetter === " " ? "▪" : newLetter}</span>`;
-    errorCount ++;
-    console.log(errorCount);
+    errorCount ++;    
   }
 
   // check if given question text is equal to user typed text
