811faf9b1c71533eda191758122a8b1e13733873
nakibuddin <nakibaust137@gmail.com>
2022-09-14 21:29:57 +0600

    countdoun start issue solved

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const display = document.getElementById("display");
 const question = document.getElementById("question");
-const startBtn = document.getElementById("start");
+const startBtn = document.getElementById("starts");
 const countdownOverlay = document.getElementById("countdown");
 const resultModal = document.getElementById("result");
 const modalBackground = document.getElementById("modal-background");
@@ -107,7 +107,7 @@ const start = () => {
   countdownOverlay.style.display = "flex";
 
   const startCountdown = setInterval(() => {
-    countdownOverlay.innerHTML = '<h1>${count}</h1>';
+    countdownOverlay.innerHTML = `<h1>${count}</h1>`;
 
     // finished timer
     if (count == 0) {
@@ -124,9 +124,8 @@ const start = () => {
 };
 
 // START Countdown
-// document.getElementById('starts').addEventListener('click', function(){});
-document.getElementById('starts').addEventListener('click', start);
-// startBtn.addEventListener("click", start);
+startBtn.addEventListener("click", start);
+
 
 // If history exists, show it
 displayHistory();
